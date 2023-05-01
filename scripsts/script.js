f5b5e91b1614545e746ad5a6804f3624bdbbdeb6
dariomiguel <101842755+dariomiguel@users.noreply.github.com>
2023-05-01 11:40:10 -0300

    Se agregó la opción:5 de busquedad y se cambió: _Se eliminó la variable index por que no era necesaria. _Se modifico el menu inicio. _Se agregaron funciones relativas a la opción de busquedad. _Se agregó la función que valida el prompt para que no pueda ser mandado en blanco.

--- scripsts/script.js
+++ scripsts/script.js
@@ -3,7 +3,7 @@
 /////////////////////////////////////////////////////////////////
 
 //Variables Globales
-let opc, cont = 0, index=0, mayor=0, mayorIndex, total=0, promedio=0, datos = false;
+let opc, mayor=0, mayorIndex, total=0, promedio=0, datos = false;
 
 //Declaración del array vacío que nos servirá para listar los elementos como si fuera una base de datos.
 let registro =  [];
@@ -15,11 +15,11 @@ let registro =  [];
 //Función para el Menú Principal que se muestra en pantalla.
 function menu(){
     opc = prompt(`MENU PRINCIPAL
-    1_Ingresar datos de máquinas.
-    2_Lista de datos.
-    3_Enviar Correo de informe diario.
-    4_Enviar Correo para máquinas que requieren mantenimiento.
-    5_Buscar máquinas por nombre de operario.\n6_SALIR\n
+    1_Ingresar datos de máquinas
+    2_Lista de datos
+    3_Enviar Correo de informe diario
+    4_Enviar Correo para máquinas que requieren mantenimiento
+    5_Buscar\n\n6_SALIR
 Seleccione una opción:`);
     opc = parseInt(opc);
 }
@@ -29,11 +29,11 @@ function ingDat(){
     //Declaración del objeto vacío que contendrá las características que se quieren almacenar.
     let maquina = {};
 
-    maquina.cod = prompt("código de máquina: \n");
-    maquina.cantPro = prompt("Ingrese cantidad de productos fabricados: \n");
-    maquina.hsPro = prompt ("Ingrese cantidad de horas de producción: \n");
-    maquina.parTec = prompt ("Ingrese cantidad de paradas técnicas: \n");
-    maquina.opeCar = prompt ("Operario a cargo: \n");
+    maquina.cod = promptValido("código de máquina: \n");
+    maquina.cantPro = promptValido("Ingrese cantidad de productos fabricados: \n");
+    maquina.hsPro = promptValido ("Ingrese cantidad de horas de producción: \n");
+    maquina.parTec = promptValido ("Ingrese cantidad de paradas técnicas: \n");
+    maquina.opeCar = promptValido ("Operario a cargo: \n");
 
     registro.push(maquina);
 }
@@ -134,15 +134,141 @@ function envMan(){
     "Informe: \n" + auxOraB  + " mantenimiento preventivo" + aux + ".\n\nSaludos Cordiales");
 }
 
-//Función para buscar según nombre de operario
-// function 
-//Teniendo en cuenta que el código de máquina de producción no diferencia entre mayúsculas y minúsculas se convierte todo a mayúsculas para poder usar el buscador
+//Función para buscar según código de máquina.
+function busCod(){
+    let buscar="", contBusquedad = 0, auxBusquedad = [];
+    //Convertimos en un string, y lo pasasmos a mayúsculas para que no haya errores en la busquedad
+    buscar = String(promptValido("Escriba el codigo de máquina que desea buscar")).toUpperCase();
+    /*Busca en el array registro, la sección del código de máquina hasta encontrar coincidencias. Si existen coincidencias 
+    se guardan  en el array auxiliar la posición en donde se encontró, y el contador de busquedad se incrementa en uno.*/
+    for(let i=0; i < registro.length; i++ ){
+        if( buscar == registro[i].cod.toUpperCase()){
+            auxBusquedad.push(i);
+            contBusquedad++;
+        }
+    }
+    /*En esta sección de la función se usa el contador de busquedad, si es cero significa que no existieron coincidencias
+    de otra forma devuelve de uno en uno, mediante alertas, el objeto alojado en la posición donde se encontró, utilizando el 
+    array auxiliar de busquedad*/
+    if (contBusquedad == 0){
+        alert("No se encontraron coincidencias.");
+    }else{
+        alert("Se encontraron las siguientes coincidencias: \n");
+        for(let i=0; i < auxBusquedad.length;i++){
+            alert(
+            "Máquina " + registro[auxBusquedad[i]].cod + ":" + "\n" +
+            "    Producción:           " + registro[auxBusquedad[i]].cantPro + "\n" + 
+            "    Horas de Trabajo: " + registro[auxBusquedad[i]].hsPro + "\n" + 
+            "    Paradas Técnicas:  " + registro[auxBusquedad[i]].parTec + "\n" +
+            "    Operario a cargo: " + registro[auxBusquedad[i]].opeCar);
+        }          
+    }
+}
+
+//Función para buscar según nombre de operario.
+function busNom(){
+    mayorP();
+    //La lógica de está función es similar a la de buscar por código.
+    let buscar="", contBusquedad = 0, auxBusquedad = [];
+    buscar = String(promptValido("Escriba el nombre del operario que desea buscar"));
+    for(let i=0; i < registro.length; i++ ){
+        if( buscar == registro[i].opeCar){
+            auxBusquedad.push(i);
+            contBusquedad++;
+        }
+    }
+    if (contBusquedad == 0){
+        alert("No se encontraron coincidencias.");
+    }else{
+        alert("Se encontraron las siguientes coincidencias: \n");
+        for(let i=0; i < auxBusquedad.length;i++){
+            alert(
+            "Máquina " + registro[auxBusquedad[ind]].cod + ":" + "\n" +
+            "    Producción:           " + registro[auxBusquedad[i]].cantPro + "\n" + 
+            "    Horas de Trabajo: " + registro[auxBusquedad[i]].hsPro + "\n" + 
+            "    Paradas Técnicas:  " + registro[auxBusquedad[i]].parTec + "\n" +
+            "    Operario a cargo: " + registro[auxBusquedad[i]].opeCar);
+        }          
+    }
+}
+
+//Función para buscar según mayor número de producción.
+function busMayP(){
+    mayorP();
+    //Reutilizando la función de mayorP, podemos obtener la posición en donde está ubicado la máquina con mayor producción.
+    alert("La máquina con mayor producción fue:\n" +
+        "Máquina " + registro[mayorIndex].cod + ":" + "\n" +
+        "    Producción:           " + registro[mayorIndex].cantPro + "\n" + 
+        "    Horas de Trabajo:  " + registro[mayorIndex].hsPro + "\n" + 
+        "    Paradas Técnicas:  " + registro[mayorIndex].parTec + "\n" +
+        "    Operario a cargo:  " + registro[mayorIndex].opeCar);
+}
+
+//Función para buscar por el mayor número de paradas.
+function busPar(){
+    mayorP();
+    let aux = 0, auxInd = 0;
+    /*En el siguiente for escanea las propiedas del objetos parada técnica, si es mayor que aux, aux cambiará a ese nuevo valor
+    y se guarda el indice de posición, de donde se encontró, para poder llamarlo despues en el alert.*/
+    for(let i = 0; i < registro.length; i++){
+        if(registro[i].parTec > aux){
+            aux = registro[i].parTec;
+            auxInd = i;
+        }
+    }
+    alert("La máquina con mayor número de paradas fue:\n" +
+        "Máquina   " + registro[auxInd].cod + ":" + "\n" +
+        "    Producción:           " + registro[auxInd].cantPro + "\n" + 
+        "    Horas de Trabajo:  " + registro[auxInd].hsPro + "\n" + 
+        "    Paradas Técnicas:  " + registro[auxInd].parTec + "\n" +
+        "    Operario a cargo:  " + registro[auxInd].opeCar);
+}
+
+//Función Busquedad
+function busquedad(){
+    debugger;
+    let indexBuscar = "";
+    do{
+        indexBuscar = prompt(`Indique de que forma quiere buscar:
+    1_Buscar por Código de máquina.
+    2_Buscar por operario.
+    3_Buscar el de mayor producción.
+    4_Buscar el que mayor paradas técnicas tuvo.
+    
+(V)olver al menú anterior`);
+        switch(indexBuscar){
+            case "1":
+                busCod();
+                break;
+            case "2":
+                busNom()
+                break;
+            case "3":
+                busMayP()
+                break;
+            case "4":
+                busPar()
+                break;
+            case "v":
+            case "V":
+                break;
+            default:
+                alert("No es una opción valida");
+                break;            
+        }
+    //Pasamos la variable a minúsculas para que no haya inconvenientes con la coincidencias
+    }while(indexBuscar.toLowerCase() != "v");
+}
 
 
-
-
-//Función para que no inicie hasta que tenga un dato cargado 
-
+//Función para que no avance hasta que tenga un dato cargado 
+function promptValido(mensaje) {
+    let valor;
+    do {
+        valor = prompt(mensaje);
+    } while (valor === "");
+    return valor;
+}
 
 
 
@@ -183,7 +309,7 @@ do{
             break;
         case 5:
             if(datos){
-                envCor();
+                busquedad();
             }else{
                 alert("No hay datos cargados todavía");
             }
